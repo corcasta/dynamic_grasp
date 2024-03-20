d159f4a397a5b2b6f237d1a9b11cc602d4a7e01d
corcasta <corcasta@outlook.com>
2024-03-20 19:34:42 +0800

    making gripper package better

--- gripper_demo.py
+++ gripper_demo.py
@@ -1,10 +1,11 @@
-from smart_gripper_pkg.scripts import Gripper
+import os
+import sys
+from smart_gripper_pkg.scripts import utilities
+from smart_gripper_pkg.scripts.gripper_interface import Gripper
 
 def main():
     import argparse
     sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
-
-    import utilities
 
     # Parse arguments
     parser = argparse.ArgumentParser()
@@ -14,7 +15,7 @@ def main():
     with utilities.DeviceConnection.createTcpConnection(args) as router:
 
         example = Gripper(router)
-        example.gripper_width(0.545)        # example.gripper_vel(-0.1)
+        example.gripper_width(0)        # example.gripper_vel(-0.1)
 
 if __name__ == "__main__":
     main()
